432235c621d4b57b0a2d99b4659cdcec732cceea
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2016-08-07 22:44:30 +0200

    Sfx Prefab & Sound Killer

---
+++ Assets/Scripts/SoundKiller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundKiller : MonoBehaviour {
+
+    private AudioSource source = null;
+
+    void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+    }
+	// Update is called once per frame
+	void Update () 
+    {
+        if (!source.isPlaying)
+            Destroy(this.gameObject);
+	}
+}
